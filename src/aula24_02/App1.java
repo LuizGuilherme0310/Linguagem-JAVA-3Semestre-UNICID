1e7e9cc93d9b92cef89e9a6e01a2a9aac7e56d9f
LuizGuilherme0310 <luigui2004outlook.com@gmail.com>
2026-02-24 20:51:34 -0300

    Primeira aula Java

---
+++ src/aula24_02/App1.java
@@ -0,0 +1,5 @@
+package aula24_02;
+
+public class App1 {
+
+}
---
+++ src/aula24_02/HelloWorld.java
@@ -0,0 +1,18 @@
+package aula24_02;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=30;
+		
+		int soma = (a+b);
+				
+	
+		System.out.println("Resultado..." + soma);
+		
+
+	}
+
+}
