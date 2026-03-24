fb5e1efaf09f01441529c422de201f0fba195931
LuizGuilherme0310 <luigui2004outlook.com@gmail.com>
2026-03-24 20:06:08 -0300

    aula 03

--- aula2403_/src/aula2403_/Pessoas.java
+++ aula2403_/src/aula2403_/Pessoas.java
@@ -2,6 +2,20 @@ package aula2403_;
 
 public class Pessoas {
 	//atributos da classe >> caracteristicas
-	int idade;
-	String nome;	
+	// privados // encapsulamento
+	private int idade;
+	private String nome;	
+	// getters e setters
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 }
--- aula2403_/src/aula2403_/Principal.java
+++ aula2403_/src/aula2403_/Principal.java
@@ -5,10 +5,12 @@ public class Principal {
 	public static void main(String[] args) {
 		// chama uma classe >> instanciar a classe
 		Pessoas p = new Pessoas();
-		int a = 10;
-
-		System.out.println("Nome....." +p.nome);
-		System.out.println("Idade....." +p.idade);
+		// atualizar os dados
+		p.setNome("Ana");
+		p.setIdade(20);
+		//imprimir
+		System.out.println("Nome....." +p.getNome());
+		System.out.println("Idade....." +p.getIdade());
 		
 	}
 
